dc3197ca400ef454e4b51925307193ca611509c7
Rajesh <sheelamrajesh0409@gmail.com>
2021-08-07 05:52:32 +0530

    first commit

---
+++ src/main/java/com/springboot/docker/api/DemoApplication.java
@@ -0,0 +1,21 @@
+package com.springboot.docker.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class DemoApplication {
+
+	@GetMapping("/message")
+	public String getMessage() {
+		return "Spring Boot Docker Example";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
